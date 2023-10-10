857147d782b5ce60fcebe878bfb11dc9e9b169d7
keitoru <keitoru@hotmail.com>
2023-10-10 10:30:21 +0800

    20231010

---
+++ 104. 二叉树的最大深度/104.go
@@ -0,0 +1,28 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+// 递归
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+}
+
+func main() {
+
+}
---
+++ 226. 翻转二叉树/226.go
@@ -0,0 +1,19 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	tmp := root.Left
+	root.Left = invertTree(root.Right)
+	root.Right = invertTree(tmp)
+
+	return root
+}
